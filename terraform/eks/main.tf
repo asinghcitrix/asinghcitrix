bc49bbf4aed2a25aa3a63829af3f111e27b0708b
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-17 17:32:52 +0530

    Update main.tf

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,1 +1,27 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.1.0"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+    }
+  }
+}
+
+data "aws_eks_cluster" "main" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = module.eks.cluster_id
+}
 

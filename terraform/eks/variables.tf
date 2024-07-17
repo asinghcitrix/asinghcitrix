10e6a6e908aa6aa00d876f363dbeec3e783808d9
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-17 17:33:34 +0530

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,1 +1,10 @@
 
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  default     = "my-cluster"
+}
+
+variable "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  default     = "1.21"
+}

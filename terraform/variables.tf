cd2b6bcf088819fb5a6770ae3f13337a8822b1b8
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-17 17:27:24 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
 
+variable "region" {
+  description = "The AWS region to deploy to."
+  default     = "us-west-2"
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster."
+  default     = "my-cluster"
+}
+
+variable "cluster_version" {
+  description = "The Kubernetes version for the EKS cluster."
+  default     = "1.21"
+}

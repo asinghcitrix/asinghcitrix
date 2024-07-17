d4ff4543a2993790d7030cf1e80d68d7c1f62a5f
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-17 17:34:17 +0530

    Update outputs.tf

--- terraform/eks/outputs.tf
+++ terraform/eks/outputs.tf
@@ -1,1 +1,10 @@
+output "cluster_endpoint" {
+  description = "The endpoint of the EKS cluster."
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "The security group ID of the EKS cluster."
+  value       = module.eks.cluster_security_group_id
+}
 

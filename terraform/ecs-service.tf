5630a2a0d00348bf298ed3aa0addbef7235a9357
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-18 18:38:35 +0530

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,12 +1,12 @@
 resource "aws_ecs_service" "service" {
-  name            = "devops-fortune-service"  # Replace with your hardcoded value if needed
+  name            = "devops-fortune-service"  
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    subnets          = ["subnet-0cc71f36f26b6b03a", "subnet-01da053285ed1019b"]  # Replace with your subnet IDs
-    security_groups  = ["sg-089c907eb24429082"]  # Replace with your security group ID
+    subnets          = ["subnet-0cc71f36f26b6b03a", "subnet-01da053285ed1019b"]  
+    security_groups  = ["sg-089c907eb24429082"]  
     assign_public_ip = true
   }
 }

9d882519305bc5be1b86239971eba00fd5b39783
asinghcitrix <172363309+asinghcitrix@users.noreply.github.com>
2024-07-18 18:39:52 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
   description = "The AWS region to deploy to"
-  default     = "us-east-2"  # Replace if needed
+  default     = "us-east-2"  
 }
 
 variable "ecr_repository_url" {
   description = "The URL of the ECR repository"
-  default     = "637423421797.dkr.ecr.us-east-2.amazonaws.com/devops-fortune-api"  # Replace if needed
+  default     = "637423421797.dkr.ecr.us-east-2.amazonaws.com/devops-fortune-api" 
 }
 
 variable "cluster_name" {
   description = "The name of the ECS cluster"
-  default     = "devops-fortune-cluster"  # Replace if needed
+  default     = "devops-fortune-cluster"  
 }
 
 variable "service_name" {
   description = "The name of the ECS service"
-  default     = "devops-fortune-service"  # Replace if needed
+  default     = "devops-fortune-service"  
 }
 
 variable "task_family" {
   description = "The family name of the ECS task definition"
-  default     = "devops-fortune-task"  # Replace if needed
+  default     = "devops-fortune-task"  
 }
